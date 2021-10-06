b29e95098637e90ba4d2a84f81f6caf5d01e5e49
田辺　哲也 <tanabetetsuya@tanabetuyanoMBP.elecom>
2021-10-06 19:01:26 +0900

    [Add] 関数getJShandの作成

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 let user_hand = prompt("好きな手をグー、チョキ、パーから選んでください");
 
 alert("あなたが選んだ手は" + user_hand + "です。"); 
+
+function getJShand(){
+  let js_hand_num = Math.floor(Math.random()*3);
+
+  if(js_hand_num == 0){
+    js_hand = "グー";
+  }else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  }else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+
+  return js_hand;
+}
